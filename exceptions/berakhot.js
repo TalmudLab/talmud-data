3fdf7cdc704628a3bfe79a852f3d8a2b212b5530
Dan Jutan <danjutan@gmail.com>
2021-01-28 09:45:29 -0500

    remove debugger statement

--- exceptions/berakhot.js
+++ exceptions/berakhot.js
@@ -1,9 +1,19 @@
 export default {
+  "2b": {
+    rashi (sefaria, hb) {
+      //The third rashi that sefaria has isn't there on Hebrewbooks - interestingly, both sources have it as a Tosafot
+      const copy = [...sefaria]
+      copy.splice(2, 1);
+      return {
+        sefaria: copy,
+        hb
+      }
+    }
+  },
   "5b": {
     rashi (sefaria, hb) {
       //rashi order discrepancy
       const swapped = [...sefaria.slice(0, 12), sefaria[14], sefaria[13], ...sefaria.slice(15)]
-      console.log("GOT HERE!")
       return {
         sefaria: swapped,
         hb
