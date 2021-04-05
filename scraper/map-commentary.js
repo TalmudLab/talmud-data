e1c04f7afd79f1672b01feff3dde37e333e8696e
Dan Jutan <danjutan@gmail.com>
2021-04-05 12:43:32 -0400

    I'm pushing this as basically just a record of my tracks. data's in the database

--- scraper/map-commentary.js
+++ scraper/map-commentary.js
@@ -38,13 +38,18 @@ async function loadCommentaries() {
   console.log("done");
 }
 
-(async () => mapJsonToMongo())();
+(async () => mapJsonToExisting())();
 
-function getClosestCandidate(compareTo, candidates) {
+function getClosestCandidate(compareTo, candidates, split = true) {
   if (!candidates.length) return false;
   const different = candidates.map( ([ref, text]) => {
-    const split = text.split(/[–-]/g);
-    const compareBody = (split.length > 1) ? split.slice(1).join("") : text;
+    let compareBody;
+    if (split) {
+      const split = text.split(/[–-]/g);
+      compareBody = (split.length > 1) ? split.slice(1).join("") : text;
+    } else {
+      compareBody = text.replaceAll(/ [–-]/g, ".").replaceAll(/\([^()]*\)/g, "");
+    }
     const diffed = stringSimilarity(compareTo, compareBody);
     return {
       ref,
@@ -54,11 +59,75 @@ function getClosestCandidate(compareTo, candidates) {
   const closest = different[0];
   console.log(closest.score, different[1]?.score);
   if (closest.score < 0.8) {
+    if (compareTo.length <= 20 && closest.score > 0.5) {
+      return closest;
+    }
+    debugger;
     return false;
   }
   return closest;
 }
 
+async function mapRashi() {
+  const dirPath = "../output/"
+  const filenames = (await readdir(dirPath)).sort( (a, b) => parseInt(a.split("-")[1]) - parseInt(b.split("-")[1]));
+  const tractate = "Berakhot"
+  const rashiEntries = Object.entries(JSON.parse(await readFile(dirPath + tractate + "-rashi.json")));
+  const entriesForDaf = (daf) => {
+    const dafstr = `${tractate}.${daf}`;
+    return rashiEntries.filter( ([ref, text]) =>
+      ref.includes(dafstr + 'a') || ref.includes(dafstr + 'b'));
+  }
+  const rashiMap = {};
+  for (const filename of filenames) {
+    if (!filename.includes(tractate)) continue;
+    const content = JSON.parse(await readFile(dirPath + filename));
+    if (!content.rashi) continue;
+    const headerRegex = /(\{[^\{\}]+\})+/g
+    const rashiSplit = content.rashi
+      .split(headerRegex)
+      .filter(t => t.trim())
+    let rashis = []
+    let nextRashi = ""
+    rashiSplit.forEach(str => {
+     if (str.includes("{") || str.includes("}")) {
+       nextRashi += str.replaceAll("{", "").replaceAll("}", "")
+     } else {
+       nextRashi += str;
+       rashis.push(nextRashi);
+       nextRashi = "";
+     }
+    })
+    rashis = rashis.map (rashi => rashi.replaceAll("<br>", " ").replaceAll(/\([^()]*\)/g, ""));
+    console.log(filename);
+    const filenameSplit = filename.split('-');
+    const daf = parseInt(filenameSplit[1]);
+    rashiMap[filename] = [];
+    rashis.forEach( (rashi, index) => {
+      let closest;
+      let candidates = entriesForDaf(daf);
+      closest = getClosestCandidate(rashi, candidates, false)
+      if (!closest) {
+        candidates = entriesForDaf(daf + 1);
+        closest = getClosestCandidate(rashi, candidates, false)
+        if (!closest) {
+          candidates = entriesForDaf(daf - 1);
+          closest = getClosestCandidate(rashi, candidates, false)
+        }
+      }
+      if (closest) {
+        rashiMap[filename].push(closest.ref);
+        const index = rashiEntries.findIndex (([ref, text]) => ref == closest.ref);
+        rashiEntries.splice(index, 1);
+      } else {
+        rashiMap[filename].push(false);
+      }
+    })
+    await writeFile(`../output/${tractate}-rashiMap.json`, JSON.stringify(rashiMap));
+  }
+  await writeFile(`../output/${tractate}-rashiMap.json`, JSON.stringify(rashiMap));
+  console.log("done");
+}
 async function mapTosafot() {
   const dirPath = "../output/"
   const filenames = (await readdir(dirPath)).sort( (a, b) => parseInt(a.split("-")[1]) - parseInt(b.split("-")[1]));
@@ -126,4 +195,27 @@ async function mapJsonToMongo() {
   )
   await writeFile(`../output/Berakhot-tosafotMap-mongo.json`, JSON.stringify(newFile));
   debugger;
+}
+
+async function mapJsonToExisting() {
+
+  const obj = JSON.parse(await readFile("../output/Berakhot-rashiMap.json"))
+  const existing = JSON.parse(await readFile("../output/Berakhot-tosafotMap-mongo.json"));
+  const entries = Object.entries(obj);
+  entries.forEach( ([page, arr]) => {
+    const split = page.split(".")[0].split("-");
+    const tractate = split[0];
+    const daf = split[1];
+    const insert = existing.findIndex(entry => entry.tractate == tractate && entry.daf == daf);
+    if (existing[insert]) {
+      existing[insert].rashiMap = arr;
+    } else {
+      existing.push({
+        tractate,
+        daf,
+        rashiMap: arr
+      })
+    }
+  })
+  await writeFile(`../output/Berakhot-commentaryMap-mongo.json`, JSON.stringify(existing));
 }
